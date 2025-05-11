8b41399c15ee9ee6f67730c356dabc61ea335c39
Dairene <123425883+dgarcia45@users.noreply.github.com>
2025-05-11 17:12:07 +0200

    Update UserRepository.java

--- back/src/main/java/net/javaguides/invest/repository/UserRepository.java
+++ back/src/main/java/net/javaguides/invest/repository/UserRepository.java
@@ -1,8 +1,13 @@
 package net.javaguides.invest.repository;
 
-import net.javaguides.invest.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.javaguides.invest.model.User;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
